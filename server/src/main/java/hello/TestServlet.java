3c91e4734eaf01bab9d1ed3d681f5aff82d7a8f5
yanJuicy <hanjub3@gmail.com>
2023-05-11 21:30:42 +0900

    WAR 빌드와 배포

---
+++ server/src/main/java/hello/TestServlet.java
@@ -0,0 +1,19 @@
+package hello;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("TestService.service");
+        resp.getWriter().println("test");
+    }
+}
